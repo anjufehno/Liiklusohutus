5e8d79f0ffaa0b8d85c9490071d3dbfcd1a2764f
anjufehno <lizakrug@tlu.ee>
2025-03-17 13:54:24 +0200

    Design update

--- script.js
+++ script.js
@@ -3,27 +3,39 @@ import { brakeDistance } from "./brakedistance.js"
 
 const roadContainer = document.querySelector(".roadContainer")
 const speedContainer = document.querySelector(".speedContainer")
-const surfaceContainer = document.querySelector(".surfaceContainer")
+
 start.disabled=true
 
 let speed=0
-let surface=0
+let surface = 0;
+const surfaceLabel = document.querySelector('label[for="surfaceContainer"]');
 
-const arrSurface=[0.8, 0.5, 0.25, 0.1]
-const buttonsSurface=[]
+const arrSurface = [
+    { value: 0.8, text: "Kuiv tee" },
+    { value: 0.5, text: "Märg tee" },
+    { value: 0.25, text: "Lumine tee" },
+    { value: 0.1, text: "Jäätee" }
+];
+
+const buttonsSurface = [];
+const surfaceContainer = document.querySelector(".surfaceContainer");
+
 arrSurface.forEach(su => {
-    const btn = document.createElement("button")
-    buttonsSurface.push(btn)
-    btn.textContent=su
-    surfaceContainer.append(btn)
-    btn.onclick= function(){
-        surface=su
-        if(speed) start.disabled=false
-        const active=surfaceContainer.querySelector(".active")
-        active?.classList.remove("active")
-        btn.classList.add("active")
-    }
+    const btn = document.createElement("button");
+    btn.dataset.coeff = su.value;
+    buttonsSurface.push(btn);
+    surfaceContainer.append(btn);
+
+    btn.onclick = function () {
+        surface = su.value;
+        surfaceLabel.textContent = `Valige teekate: ${su.text}`; // Обновляем текст label
+        if (speed) start.disabled = false;
+        const active = surfaceContainer.querySelector(".active");
+        active?.classList.remove("active");
+        btn.classList.add("active");
+    };
 });
+
 
 const arrSpeed=[30,40,50,60,80,100,120]
 const buttonsSpeed=[]
